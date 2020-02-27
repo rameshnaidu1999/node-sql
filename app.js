299f57f3a2aeecb64098baa8d4069acca41ada44
Ramesh Naidu <reengineer1999@gmail.com>
2020-02-27 14:08:10 +0530

    connected to db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mysql = require('mysql');
+const path = require('path');
 
 // Init Express app
 const app = express();
@@ -13,23 +14,42 @@ const con = mysql.createConnection({
   database : 'test'
 });
 
-// DB message
-con.connect((function(err) {
-  if (err) throw err;
-  
-  console.log('connected to MySQL server, ');
-}));
-
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
 
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+// DB message
+let obj = {};
+con.connect(function (err, req,res) {
+    if(err) {
+        console.log(err);
+    } else
+        console.log('Connected to Server');
+        // Mysql Query
+        con.query("SELECT * FROM products", function (err, result) {
+            if(err) throw err;
+            let obj = { obj : result}
+            // app.use(path.join(__dirname, 'index'), require('./routes/index'));
+            console.log(result);
+            
+        });
+})
+
+app.listen(3001, () => {
+    console.log('Example app listening on port 3001!');
 });
 
 //Run app, then load http://localhost:port in a browser to see the output.
+
+// <!-- <% for(var i=0; i < rows.length; i++) { %>
+//     <tr>
+//       <td><%= rows[i].id %></td>
+//       <td><%= rows[i].user %></td>
+//       <td><%= rows[i].PASS %></td>
+//     </tr>
+//  <% } %> -->
